86bca204c024115c7297ff2a983e374eb879e3c5
Kyrix <75959500+kyrixhan@users.noreply.github.com>
2021-08-07 22:42:39 +0900

    1추자 모의고사 추가

---
+++ Programmers Study/Week 1/나머지 한 점.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution (vector<vector<int>> v) {
+    vector<int> answer = {0, 0};
+
+    for (int i = 0; i < 3; i++) {
+        answer[0] ^= v[i][0];
+        answer[1] ^= v[i][1];
+    }
+
+    return answer;
+}
