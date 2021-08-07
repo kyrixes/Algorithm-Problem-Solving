343681982e4e95de15968dc2c39f692bc6c560cb
Kyrix <75959500+kyrixhan@users.noreply.github.com>
2021-08-07 22:36:06 +0900

    짝지어 제거하기 업데이트

--- Programmers Study/사전 테스트/짝지어 제거하기.cpp
+++ Programmers Study/사전 테스트/짝지어 제거하기.cpp
@@ -1,44 +1,15 @@
-#include <vector>
-#include <numeric>
-#include <cmath>
+#include <string>
+#include <stack>
 
 using namespace std;
 
-vector<int> primes;
-
-bool isPrime (int n) {
-    for (int i = 2; i <= sqrt(n) && n > 1; i++) {
-        if (n % i == 0) {
-            return false;
-        }
+int solution(string s) {
+    stack<char> stk;
+    
+    for (char c : s) {
+        if (stk.empty() || stk.top() != c) stk.push(c);
+        else stk.pop();
     }
     
-    return true;
-}
-
-void pick (vector<int>& nums, vector<int>& picked, int index) {
-    if (picked.size() == 3) {
-        primes.push_back(accumulate(picked.begin(), picked.end(), 0));
-        return;
-    }
-    
-    for (int i = index; i < nums.size(); i++) {
-        picked.push_back(nums[i]);
-        pick(nums, picked, i + 1);
-        picked.pop_back();
-    }
-}
-
-int solution(vector<int> nums) {
-    int answer = 0;
-    
-    vector<int> picked;
-    
-    pick(nums, picked, 0);
-    
-    for (int i = 0; i < primes.size(); i++) {
-        answer += isPrime(primes[i]);
-    }
-    
-    return answer;
+    return (stk.empty());
 }
